3daffcada2b78219ac7f15fd4a49e1bd49c542e7
pmpurcell93 <maddenpurcell@gmail.com>
2021-10-17 21:31:20 -0500

    Adding routing and auth to react app

---
+++ src/views/Completed.js
@@ -0,0 +1,38 @@
+import { React, useState, useEffect } from 'react';
+import { deleteCompleteTodo, getCompletedTodos } from '../api/data/todoData';
+
+export default function Completed() {
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  useEffect(() => {
+    getCompletedTodos().then(setCompleteTodos);
+  }, []);
+
+  const handleClick = (key) => {
+    deleteCompleteTodo(key).then(setCompleteTodos);
+  };
+
+  return (
+    <div>
+      <h1>Welcome to Completed!</h1>
+      <div>
+        {completeTodos.map((todo) => (
+          <div
+            className="d-flex justify-content-between alert alert-light"
+            role="alert"
+            key={todo.firebaseKey}
+          >
+            {todo.name}
+            <button
+              onClick={() => handleClick(todo.firebaseKey)}
+              className="btn btn-danger"
+              type="button"
+            >
+              DELETE
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
